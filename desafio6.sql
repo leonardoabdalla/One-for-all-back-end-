84c6ccca058c5e47e27cf67f38856e62015dc07b
Leonardo Abdalla <leonardoabdalladeveloper@gmail.com>
2022-05-12 00:09:40 -0300

    Requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,5 +1,5 @@
 SELECT ROUND((MIN(p.plano_valor)), 2) AS faturamento_minimo,
-MAX(p.plano_valor) AS faturamento_maximo,
+ROUND((MAX(p.plano_valor)), 2) AS faturamento_maximo,
 ROUND((AVG(p.plano_valor)), 2) AS faturamento_medio,
 ROUND((SUM(p.plano_valor)), 2) AS faturamento_total
 FROM SpotifyClone.plano AS p
