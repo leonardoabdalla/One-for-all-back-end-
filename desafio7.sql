410ef2133e9c50def56f547b0c46a27133d19b52
Leonardo Abdalla <leonardoabdalladeveloper@gmail.com>
2022-05-12 10:06:56 -0300

    Requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT art.artista_name AS "artista", alb.album_name AS "album", COUNT(sa.seguidor_ida) AS seguidores
+FROM SpotifyClone.artista AS art
+INNER JOIN SpotifyClone.album AS alb
+ON art.artista_id = alb.artista_id
+INNER JOIN SpotifyClone.seguindo_artistas AS sa
+ON art.artista_id = sa.seguido_ida
+GROUP BY art.artista_name, alb.album_name
+ORDER BY seguidores DESC;
