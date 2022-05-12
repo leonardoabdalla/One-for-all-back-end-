a53036606a3bbe1f01de350dcdbe0c367f55a478
Leonardo Abdalla <leonardoabdalladeveloper@gmail.com>
2022-05-11 23:48:19 -0300

    Requisito 6

---
+++ desafio6.sql
@@ -0,0 +1,7 @@
+SELECT MIN(p.plano_valor) AS faturamento_minimo,
+MAX(p.plano_valor) AS faturamento_maximo,
+ROUND((AVG(p.plano_valor)), 2) AS faturamento_medio,
+ROUND((SUM(p.plano_valor)), 2) AS faturamento_total
+FROM SpotifyClone.plano AS p
+INNER JOIN SpotifyClone.usuario AS u
+ON p.plano_id = u.plano_id;
