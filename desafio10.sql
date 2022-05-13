22bf5ed6cab69d14925205167844e78108fe5535
Leonardo Abdalla <leonardoabdalladeveloper@gmail.com>
2022-05-12 22:49:36 -0300

    Requisito 10

---
+++ desafio10.sql
@@ -0,0 +1,11 @@
+SELECT m.musica_name AS "nome", COUNT(h.musica_idh) AS "reproducoes"
+FROM SpotifyClone.musicas AS m
+INNER JOIN SpotifyClone.historico_reproducoes AS h 
+ON m.musica_id = h.musica_idh
+INNER JOIN SpotifyClone.usuario AS u
+ON h.usuario_idh = u.usuario_id
+INNER JOIN SpotifyClone.plano AS p
+ON u.usuario_id = p.plano_id
+WHERE p.plano_name = "gratuito" or p.plano_name = "pessoal"
+GROUP BY m.musica_name
+ORDER BY nome ASC;
